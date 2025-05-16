9777556b4d1d792768ac5c1ce1e81e2fffcf7729
Tarun Jangid <tarundjangid@gmail.com>
2025-05-16 20:26:52 +0530

    basic cleanup

--- main.cpp
+++ main.cpp
@@ -129,20 +129,20 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		glm::mat4 trans = glm::mat4(1.0f);
+		// trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
 		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 		
         glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		trans = glm::mat4(1.0f);
-		trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
-		float scaleAmount = static_cast<float>(sin(glfwGetTime()));
-		trans = glm::scale(trans, glm::vec3(scaleAmount));
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &trans[0][0]);
-
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//		trans = glm::mat4(1.0f);
+//		trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
+//		float scaleAmount = static_cast<float>(sin(glfwGetTime()));
+//		trans = glm::scale(trans, glm::vec3(scaleAmount));
+//		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &trans[0][0]);
+//
+//		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
