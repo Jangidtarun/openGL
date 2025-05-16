1bd6f73405f4e1473cfeda8506bec0e56cb09d55
Tarun Jangid <tarundjangid@gmail.com>
2025-05-16 16:04:16 +0530

    chapter on transformations exercise 1 complete i was really surprised by the result

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include "shader.h"
 #include <iostream>
 #include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -114,6 +116,8 @@ int main()
 	glUniform1i(glGetUniformLocation(ourShader.ID, "texture"), 0);
 	ourShader.setInt("texture2", 1);
 
+	unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -124,6 +128,11 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+		
         glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
