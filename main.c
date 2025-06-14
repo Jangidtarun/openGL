fad3edf8e09e3c8d5807f8a99e72e894ebb4f910
Tarun Jangid <tarundjangid@gmail.com>
2025-06-14 19:25:18 +0530

    create vertex and fragment shaders; next step: link both shaders together

--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+#define INFO_LOG_SIZE 512
+
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main() {\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "}\0";
+
+
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 fragColor;\n"
+	"void main() {\n"
+	"	fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
 
 int main() {
 	if (!glfwInit()) {
@@ -27,6 +43,46 @@ int main() {
 	}
 
 	glViewport(0, 0, 600, 600);
+
+	float vertices[] = {
+		0.0f,  0.5f, 0.0f, // Top vertex
+		-0.5f, -0.5f, 0.0f, // Bottom left vertex
+		0.5f, -0.5f, 0.0f  // Bottom right vertex
+	};
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[INFO_LOG_SIZE];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, INFO_LOG_SIZE, NULL, infoLog);
+		fprintf(stderr, "ERROR:SHADER:VERTEX:COMPILATION FAILED\n%s\n", infoLog);
+	}
+
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, INFO_LOG_SIZE, NULL, infoLog);
+		fprintf(stderr, "ERROR:SHADER:FRAGMENT:COMPILATION FAILED\n%s\n", infoLog);
+	}
+
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.2f, 0.3f, 0.3f, 0.2f);
