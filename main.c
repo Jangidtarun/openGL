03e5be836251715cd5e299f822182608cc175a60
Tarun Jangid <tarundjangid@gmail.com>
2025-06-16 08:56:05 +0530

    Create a shader program and use it to display the triangle

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 #define INFO_LOG_SIZE 512
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 600
 
 
 const char *vertexShaderSource = "#version 330 core\n"
@@ -29,7 +31,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow *window = glfwCreateWindow(800, 600, "Win1", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Win1", NULL, NULL);
 	if (!window) {
 		fprintf(stderr, "Failed to create GLFW window\n");
 		glfwTerminate();
@@ -42,7 +44,7 @@ int main() {
 		return -1;
 	}
 
-	glViewport(0, 0, 600, 600);
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	float vertices[] = {
 		0.0f,  0.5f, 0.0f, // Top vertex
@@ -51,9 +53,14 @@ int main() {
 	};
 
 	GLuint VBO;
+	GLuint VAO;
+
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 
 	GLuint vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -73,7 +80,7 @@ int main() {
 	GLuint fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-	glShaderSource(fragmentShader, 1, fragmentShaderSource, NULL);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
@@ -83,16 +90,37 @@ int main() {
 		fprintf(stderr, "ERROR:SHADER:FRAGMENT:COMPILATION FAILED\n%s\n", infoLog);
 	}
 
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, INFO_LOG_SIZE, NULL, infoLog);
+		fprintf(stderr, "ERROR:PROGRAM:SHADER:LINKING FAILED\n%s\n", infoLog);
+	}
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+	glEnableVertexAttribArray(0);
+
+	glUseProgram(shaderProgram);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.2f, 0.3f, 0.3f, 0.2f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
 	}
 
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
