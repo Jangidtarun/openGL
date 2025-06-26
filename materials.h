aa8c690c83d515a4b147192db5ab6f13bcbac22f
Tarun Jangid <tarundjangid@gmail.com>
2025-06-26 18:36:44 +0530

    create: add a new materials.h file that holds materials related info and some default materials as well

---
+++ materials.h
@@ -0,0 +1,21 @@
+#ifndef MATERIALS_H
+#define MATERIALS_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+typedef struct {
+	glm::vec3	ambient;
+	glm::vec3	diffuse;
+	glm::vec3	specular;
+	float		shininess;
+} Material;
+
+Material obsidian = {
+	glm::vec3(0.053750,	0.050000, 	0.066250),
+	glm::vec3(0.182750, 	0.170000, 	0.225250),
+	glm::vec3(0.332741, 	0.328634, 	0.346435),
+	0.3f
+};
+
+#endif
