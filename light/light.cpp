1baa36cc560a64e43a5c0bdeef339d41e4472499
Tarun Jangid <tarundjangid@gmail.com>
2025-07-01 11:31:36 +0530

    16.3 Add the attenuation feature to the poin light

--- light/light.cpp
+++ light/light.cpp
@@ -2,10 +2,13 @@
 
 PointLight
 create_point_light(glm::vec3 position,
-		glm::vec3 color,
-		glm::vec3 ambient_intensity,
-		glm::vec3 diffuse_intensity,
-		glm::vec3 specular) {
+	glm::vec3 color,
+	glm::vec3 ambient_intensity,
+	glm::vec3 diffuse_intensity,
+	glm::vec3 specular,
+	float kc,
+	float kl,
+	float kq) {
 	PointLight light;
 
 	light.position			= position;
@@ -17,16 +20,20 @@ create_point_light(glm::vec3 position,
 	light.diffuse	= light.color	* light.ambient_intensity;
 	light.ambient	= light.diffuse	* light.diffuse_intensity;
 
+	light.kc = kc;
+	light.kl = kl;
+	light.kq = kq;
+
 	return light;
 }
 
 
 DirectionalLight
 create_directional_light(glm::vec3 direction,
-		glm::vec3 color,
-		glm::vec3 ambient_intensity,
-		glm::vec3 diffuse_intensity,
-		glm::vec3 specular) {
+	glm::vec3 color,
+	glm::vec3 ambient_intensity,
+	glm::vec3 diffuse_intensity,
+	glm::vec3 specular) {
 	DirectionalLight light;
 
 	light.direction			= direction;
--- light/light.h
+++ light/light.h
@@ -11,6 +11,12 @@ const glm::vec3 DEFAULT_LIGHT_DIF_INTEN	= glm::vec3(0.5f);
 const glm::vec3 DEFAULT_LIGHT_SPE_INTEN = glm::vec3(1.0f);
 const glm::vec3 DEFAULT_LIGHT_DIRECTION = glm::vec3(0.0f, -1.0f, 0.0f);
 
+// values for distance = 50
+const float KC = 1.0f;
+const float KL = 0.09f;
+const float KQ = 0.032f;
+
+
 typedef struct {
 	// spatial information
 	glm::vec3	position;
@@ -24,6 +30,11 @@ typedef struct {
 	// light information (generated)
 	glm::vec3	ambient;
 	glm::vec3	diffuse;
+
+	// attenuation constants
+	float	kc;
+	float	kl;
+	float	kq;
 } PointLight;
 
 
@@ -48,7 +59,10 @@ create_point_light(glm::vec3 position	= DEFAULT_POINT_LIGHT_POSITION,
 	glm::vec3 color	= DEFAULT_LIGHT_COLOR,
 	glm::vec3 ambient_intensity	= DEFAULT_LIGHT_AMB_INTEN,
 	glm::vec3 diffuse_intensity	= DEFAULT_LIGHT_DIF_INTEN,
-	glm::vec3 specular			= DEFAULT_LIGHT_SPE_INTEN);
+	glm::vec3 specular			= DEFAULT_LIGHT_SPE_INTEN,
+	float kc = KC,
+	float kl = KL,
+	float kq = KQ);
 
 
 DirectionalLight
