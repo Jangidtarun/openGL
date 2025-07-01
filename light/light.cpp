ddd8484939edc72bb935add49722b6f284f61112
Tarun Jangid <tarundjangid@gmail.com>
2025-07-01 13:04:57 +0530

    create a new light type: SpotLight

--- light/light.cpp
+++ light/light.cpp
@@ -17,8 +17,41 @@ create_point_light(glm::vec3 position,
 	light.diffuse_intensity	= diffuse_intensity;
 
 	light.specular	= specular;
-	light.diffuse	= light.color	* light.ambient_intensity;
-	light.ambient	= light.diffuse	* light.diffuse_intensity;
+	light.ambient	= light.color	* light.ambient_intensity;
+	light.diffuse	= light.color	* light.diffuse_intensity;
+
+	light.kc = kc;
+	light.kl = kl;
+	light.kq = kq;
+
+	return light;
+}
+
+SpotLight
+create_spot_light(glm::vec3 position,
+	glm::vec3 direction,
+	float cutoff_angle,
+	float outer_cutoff_angle,
+	glm::vec3 color,
+	glm::vec3 ambient_intensity,
+	glm::vec3 diffuse_intensity,
+	glm::vec3 specular,
+	float kc,
+	float kl,
+	float kq) {
+	SpotLight light;
+
+	light.position			= position;
+	light.direction			= direction;
+	light.cutoff_angle		= cutoff_angle;
+	light.outer_cutoff_angle		= outer_cutoff_angle;
+	light.color				= color;
+	light.ambient_intensity	= ambient_intensity;
+	light.diffuse_intensity	= diffuse_intensity;
+
+	light.specular	= specular;
+	light.ambient	= light.color	* light.ambient_intensity;
+	light.diffuse	= light.color	* light.diffuse_intensity;
 
 	light.kc = kc;
 	light.kl = kl;
@@ -42,8 +75,8 @@ create_directional_light(glm::vec3 direction,
 	light.diffuse_intensity	= diffuse_intensity;
 
 	light.specular	= specular;
-	light.diffuse	= light.color	* light.ambient_intensity;
-	light.ambient	= light.diffuse	* light.diffuse_intensity;
+	light.ambient	= light.color	* light.ambient_intensity;
+	light.diffuse	= light.color	* light.diffuse_intensity;
 
 	return light;
 }
--- light/light.h
+++ light/light.h
@@ -10,6 +10,8 @@ const glm::vec3 DEFAULT_LIGHT_AMB_INTEN	= glm::vec3(1.0f);
 const glm::vec3 DEFAULT_LIGHT_DIF_INTEN	= glm::vec3(0.5f);
 const glm::vec3 DEFAULT_LIGHT_SPE_INTEN = glm::vec3(1.0f);
 const glm::vec3 DEFAULT_LIGHT_DIRECTION = glm::vec3(0.0f, -1.0f, 0.0f);
+const float DEFAULT_SPOTLIGHT_CUTOFF_ANGLE = 12.0f;
+const float DEFAULT_SPOTLIGHT_OUTER_CUTOFF_ANGLE = 17.0f;
 
 // values for distance = 50
 const float KC = 1.0f;
@@ -37,6 +39,29 @@ typedef struct {
 	float	kq;
 } PointLight;
 
+typedef struct {
+	// spatial information
+	glm::vec3	position;
+	glm::vec3	direction;
+	float cutoff_angle;
+	float outer_cutoff_angle;
+
+	// light information (required)
+	glm::vec3	color;
+	glm::vec3	ambient_intensity;
+	glm::vec3	diffuse_intensity;
+	glm::vec3	specular;
+
+	// light information (generated)
+	glm::vec3	ambient;
+	glm::vec3	diffuse;
+
+	// attenuation constants
+	float	kc;
+	float	kl;
+	float	kq;
+} SpotLight;
+
 
 typedef struct {
 	// spatial information
@@ -56,6 +81,20 @@ typedef struct {
 
 PointLight
 create_point_light(glm::vec3 position	= DEFAULT_POINT_LIGHT_POSITION,
+	glm::vec3 color	= DEFAULT_LIGHT_COLOR,
+	glm::vec3 ambient_intensity	= DEFAULT_LIGHT_AMB_INTEN,
+	glm::vec3 diffuse_intensity	= DEFAULT_LIGHT_DIF_INTEN,
+	glm::vec3 specular			= DEFAULT_LIGHT_SPE_INTEN,
+	float kc = KC,
+	float kl = KL,
+	float kq = KQ);
+
+
+SpotLight
+create_spot_light(glm::vec3 position	= DEFAULT_POINT_LIGHT_POSITION,
+	glm::vec3 direction	= DEFAULT_LIGHT_DIRECTION,
+	float cutoff_angle = DEFAULT_SPOTLIGHT_CUTOFF_ANGLE,
+	float outer_cutoff_angle = DEFAULT_SPOTLIGHT_OUTER_CUTOFF_ANGLE,
 	glm::vec3 color	= DEFAULT_LIGHT_COLOR,
 	glm::vec3 ambient_intensity	= DEFAULT_LIGHT_AMB_INTEN,
 	glm::vec3 diffuse_intensity	= DEFAULT_LIGHT_DIF_INTEN,
