7f9e2a4499bf338caf152074e11ededfb226a78d
Tarun Jangid <tarundjangid@gmail.com>
2025-07-14 15:37:14 +0530

    enable face culling

--- main.cpp
+++ main.cpp
@@ -74,6 +74,10 @@ int main() {
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_FRONT);
+	glFrontFace(GL_CCW);
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
